d44bdff4758fbfa5535d020df39754e22e98fb49
유현모 사원 (ITO) <kora1492@hanwha.com>
2021-08-05 09:39:11 +0900

    First Commit 20210805

---
+++ src/main/BankStatementAnalyzerSimple.java
@@ -0,0 +1,26 @@
+package main;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class BankStatementAnalyzerSimple {
+
+    private static final String RESOURCES = "src/main/resources/";
+
+    public static void main(final String[] args) throws IOException {
+            final Path path = Paths.get(RESOURCES + "bank-data-simple.csv");
+            final List<String> lines = Files.readAllLines(path);
+            double total = 0;
+            for(final String line: lines) {
+                String[] columns = line.split(",");
+                double amount = Double.parseDouble(columns[1]);
+                total += amount;
+            }
+
+            System.out.println("모든 거래내역의 합 : " + total);
+    }
+}
